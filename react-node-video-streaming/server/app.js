25cb4dd1b23de25e179972b578f21e685c348888
ivan-ahumada <ivan.ahumada@mavs.uta.edu>
2021-08-31 15:29:26 -0500

    Return metadata for list of videos

--- react-node-video-streaming/server/app.js
+++ react-node-video-streaming/server/app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const fs = require('fs'); // makes it easy to read and write to files on the server
 const path = require('path');
+const cors = require('cors');
+
+const videos = [
+    {
+        id: 0,
+        poster: '/video/0/poster',
+        duration: '3 mins',
+        name: 'Sample 1'
+    },
+    {
+        id: 1,
+        poster: '/video/1/poster',
+        duration: '4 mins',
+        name: 'Sample 2'
+    },
+    {
+        id: 2,
+        poster: '/video/2/poster',
+        duration: '2 mins',
+        name: 'Sample 3'
+    },
+];
 
 const app = express();
 
@@ -8,6 +30,9 @@ const app = express();
 app.get('/video', (req, res) => {
     res.sendFile('assets/sample.mp4', { root: __dirname });
 });
+
+app.use(cors());
+app.get('/videos', (req, res) => res.json(videos));
 
 // Sets up server to listen on port 4000
 app.listen(4000, () => {
